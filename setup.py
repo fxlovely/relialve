f26bf101c538a1557524784e37e58fa6d5826af2
fxlovely <fxlovely@openSUSE.site>
2017-11-16 11:45:54 +0800

    3rd commit

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='relialve',
       version='1.0',
       description='OpenShift App',
       author='Your Name',
-      author_email='example@example.com',
+      author_email='fxlovely@outlook.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['alembic==0.8.9', 'blinker==1.4', 'click==6.6', 'dominate==2.3.1', 'Flask==0.12', 'Flask-Bootstrap==3.3.7.0', 'Flask-Login==0.4.0', \
           'Flask-Mail==0.9.1', 'Flask-Migrate==2.0.2', 'Flask-Moment==0.5.1', 'Flask-Script==2.0.5', 'Flask-SQLAlchemy==2.1', 'Flask-Uploads==0.2.1', \
